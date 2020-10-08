6b22079f900fe9c794fd2125704d2ab38b535593
bagal-co <withbob2@gmail.com>
2020-10-08 21:32:23 +1100

    initial javascript

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+const carsoule_container = document.getElementById("carsoule__container");
+const carsoule_sliders = document.querySelectorAll(".carsoule__slider");
+const carsoule_track = document.getElementById("carsoule__track");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+console.log(carsoule_sliders);
